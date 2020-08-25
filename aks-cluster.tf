341e1fa41360f5c6a2bb555ac41737f0c1b479c3
egarelnabi <egarelnabi@users.noreply.github.com>
2020-08-25 14:49:37 -0400

    Update aks-cluster.tf

--- aks-cluster.tf
+++ aks-cluster.tf
@@ -1,4 +1,4 @@
-resource "random_pet" "prefix" {}
+//resource "random_pet" "prefix" {}
 
 variable "appId" {
 }
@@ -11,7 +11,8 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "default" {
-  name     = "${random_pet.prefix.id}-rg"
+  //name     = "${random_pet.prefix.id}-rg"
+  name     = "${var.prefix}-rg"
   location = "West US 2"
 
   tags = {
@@ -20,10 +21,12 @@ resource "azurerm_resource_group" "default" {
 }
 
 resource "azurerm_kubernetes_cluster" "default" {
-  name                = "${random_pet.prefix.id}-aks"
+  //name                = "${random_pet.prefix.id}-aks"
+  name                = "${var.prefix}-aks"
   location            = azurerm_resource_group.default.location
   resource_group_name = azurerm_resource_group.default.name
-  dns_prefix          = "${random_pet.prefix.id}-k8s"
+  //dns_prefix          = "${random_pet.prefix.id}-k8s"
+  dns_prefix          = "${var.prefix}-k8s"
 
   agent_pool_profile {
     name            = "default"
